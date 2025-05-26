1a7b506674c50615307d0b30944a91b208aa841b
Patel9094 <aksharpatel2321@gmail.com>
2025-05-26 11:48:59 +0530

    php code

---
+++ test.php
@@ -0,0 +1,7 @@
+<!-- navbar.php -->
+<nav style="background-color: #333; padding: 10px;">
+  <a href="index.php" style="color: white; margin-right: 15px;">Home</a>
+  <a href="about.php" style="color: white; margin-right: 15px;">About</a>
+  <a href="services.php" style="color: white; margin-right: 15px;">Services</a>
+  <a href="contact.php" style="color: white;">Contact</a>
+</nav>
